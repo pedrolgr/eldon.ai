ccee2cf3e0017d4a360a6f420d36711e8ba9496c
pedrolgr <pedrolgr47@gmail.com>
2026-01-19 20:37:32 -0300

    chore: added deppendencies to the code and changed to .ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import dotenv from 'dotenv';
 import { Client, Events, GatewayIntentBits } from "discord.js"
 import { joinVoiceChannel, VoiceConnection, entersState, VoiceConnectionStatus } from "@discordjs/voice";
-import 'dotenv/config'
 import { recordVoiceHandler } from "./utils/voiceHandler.js";
+
+dotenv.config();
 
 const client = new Client({
     intents: [
@@ -16,10 +18,10 @@ client.once(Events.ClientReady, async (readyClient) => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 
     const guild = await client.guilds.fetch('1461935887762980938')
-    const channel = await guild.channels.fetch('1461935888715092024')
+    const channel = await guild.channels.fetch('1461935888715092024');
 
     const connection = joinVoiceChannel({
-        channelId: channel.id,
+        channelId: channel.id,  
         guildId: guild.id,
         adapterCreator: channel.guild.voiceAdapterCreator,
         selfDeaf: false,
@@ -31,4 +33,4 @@ client.once(Events.ClientReady, async (readyClient) => {
 
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.login(process.env.DISCORD_TOKEN);
--- src/utils/voiceHandler.ts
+++ src/utils/voiceHandler.ts
@@ -1,24 +1,27 @@
-import { EndBehaviorType } from "@discordjs/voice";
+import { EndBehaviorType, VoiceReceiver } from "@discordjs/voice";
+import { User } from "discord.js";
 
 export function recordVoiceHandler(connection) {
-    const receiver = connection.receiver;
+    const receiver: VoiceReceiver = connection.receiver;
 
-    receiver.speaking.on('start', (userId) => {
+    receiver.speaking.on('start', (userId: string) => {
 
         console.log(`User ${userId} is speaking`)
 
         const stream = receiver.subscribe(userId, {
             end: {
                 behavior: EndBehaviorType.AfterSilence,
-                duration: 1_000
+                duration: 100
             }
         })
 
-        stream.on('data', () => { });
+        
 
         stream.on('end', () => {
             console.log(`User ${userId} parou de falar`);
+
         });
+        console.log(stream)
 
     });
 
